2f95bd6b008314dac69ac35202e8b593b95e36fd
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-06-02 18:47:58 -0500

    Move combine helper into its own file

--- CatsApp/CatsApp/App/SceneDelegate.swift
+++ CatsApp/CatsApp/App/SceneDelegate.swift
@@ -31,12 +31,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .eraseToAnyPublisher()
     }
 }
-
-extension URLSession {
-    func httpDataTaskPublisher<T>(for request: URLRequest, mapper: @escaping (Data, HTTPURLResponse) throws -> T) -> AnyPublisher<T, Error> {
-        dataTaskPublisher(for: request)
-                .tryMap { ($0, $1 as! HTTPURLResponse) }
-                .tryMap(mapper)
-                .eraseToAnyPublisher()
-    }
-}
