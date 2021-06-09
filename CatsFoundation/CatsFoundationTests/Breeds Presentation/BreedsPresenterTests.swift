271c79111f5b064c2b95e58f311cc10952dabc91
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-06-09 17:06:29 -0500

    Maps breed to presentable breed

--- CatsFoundation/CatsFoundationTests/Breeds Presentation/BreedsPresenterTests.swift
+++ CatsFoundation/CatsFoundationTests/Breeds Presentation/BreedsPresenterTests.swift
@@ -7,6 +7,18 @@ import XCTest
 
 struct BreedsPresenter {
     let title = "Breeds"
+
+    func map(_ breed: Breed) -> PresentableBreed {
+        PresentableBreed(
+            breedID: "id: \(breed.id)",
+            name: breed.name,
+            description: breed.description,
+            temperament: breed.temperament,
+            origin: breed.origin,
+            lifeSpan: "\(breed.lifeSpan) average life span",
+            imageURL: breed.image
+        )
+    }
 }
 
 final class BreedsPresenterTests: XCTestCase {
@@ -14,5 +26,32 @@ final class BreedsPresenterTests: XCTestCase {
         let sut = BreedsPresenter()
 
         XCTAssertEqual(sut.title, "Breeds")
+    }
+
+    func test_map_createsPresentableBreed() {
+        let sut = BreedsPresenter()
+        let breed = Breed(
+            id: "any",
+            name: "any",
+            origin: "mx",
+            description: "a description",
+            temperament: "a temperament",
+            lifeSpan: "1 - 2",
+            adaptability: 1,
+            affectionLevel: 2,
+            childFriendly: 3,
+            dogFriendly: 4,
+            image: nil,
+            weight: "3 - 5"
+        )
+
+        let presentableBreed = sut.map(breed)
+
+        XCTAssertEqual(presentableBreed.breedID, "id: \(breed.id)")
+        XCTAssertEqual(presentableBreed.name, breed.name)
+        XCTAssertEqual(presentableBreed.description, breed.description)
+        XCTAssertEqual(presentableBreed.temperament, breed.temperament)
+        XCTAssertEqual(presentableBreed.origin, breed.origin)
+        XCTAssertEqual(presentableBreed.lifeSpan, "\(breed.lifeSpan) average life span")
     }
 }
