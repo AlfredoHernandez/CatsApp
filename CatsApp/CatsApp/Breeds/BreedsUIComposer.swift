9e600c464d63d34ff847f6178010986bee72c996
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-06-09 12:44:47 -0500

    Add navigation to a empty view

--- CatsApp/CatsApp/Breeds/BreedsUIComposer.swift
+++ CatsApp/CatsApp/Breeds/BreedsUIComposer.swift
@@ -5,10 +5,11 @@
 import CatsFoundation
 import Combine
 import Foundation
+import SwiftUI
 
 class BreedsUIComposer {
     static func composeWith(loader: AnyPublisher<[Breed], Error>) -> BreedsView {
         let store = BreedStore(loader: loader)
-        return BreedsView(store: store)
+        return BreedsView(store: store, onSelect: { _ in AnyView(EmptyView()) })
     }
 }
